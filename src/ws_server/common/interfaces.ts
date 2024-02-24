5dd7a442976ab84725dddcc57f5f2dcf1d5db668
amgstrider <amgstrider@gmail.com>
2024-02-24 12:26:01 +0400

    feat: add interfaces

---
+++ src/ws_server/common/interfaces.ts
@@ -0,0 +1,164 @@
+interface Message {
+  type: string;
+  data: any;
+  id: 0;
+}
+
+export interface RegRequestMessage extends Message {
+  type: 'reg';
+  data: {
+    name: string;
+    password: string;
+  };
+}
+
+export interface RegResponseMessage extends Message {
+  type: 'reg';
+  data: {
+    name: string;
+    index: number | string;
+    error: boolean;
+    errorText: string;
+  };
+}
+
+export interface UpdateWinnersResponseMessage extends Message {
+  type: 'update_winners';
+  data: [
+    {
+      name: string;
+      wins: number;
+    }
+  ];
+}
+
+export interface CreateRoomRequestMessage extends Message {
+  type: 'create_room';
+  data: '';
+}
+
+export interface AddUserToRoomRequestMessage extends Message {
+  type: 'add_user_to_room';
+  data: {
+    indexRoom: number | string;
+  };
+}
+
+export interface CreateGameResponseMessage extends Message {
+  type: 'create_game'; //send for both players in the room
+  data: {
+    idGame: number | string;
+    idPlayer: number | string;
+  };
+}
+
+export interface UpdateRoomStateResponseMessage extends Message {
+  type: 'update_room';
+  data: [
+    {
+      roomId: number | string;
+      roomUsers: [
+        {
+          name: string;
+          index: number | string;
+        }
+      ];
+    }
+  ];
+}
+
+export interface AddShipsRequestMessage extends Message {
+  type: 'add_ships';
+  data: {
+    gameId: number | string;
+    ships: [
+      {
+        position: {
+          x: number;
+          y: number;
+        };
+        direction: boolean;
+        length: number;
+        type: 'small' | 'medium' | 'large' | 'huge';
+      }
+    ];
+    indexPlayer:
+      | number
+      | string /* id of the player in the current game session */;
+  };
+}
+
+export interface StartGameResponseMessage extends Message {
+  type: 'start_game';
+  data: {
+    ships: /* player's ships, not enemy's */
+    [
+      {
+        position: {
+          x: number;
+          y: number;
+        };
+        direction: boolean;
+        length: number;
+        type: 'small' | 'medium' | 'large' | 'huge';
+      }
+    ];
+    currentPlayerIndex: number | string;
+  };
+}
+
+export interface AttackRequestMessage extends Message {
+  type: 'attack';
+  data: {
+    gameId: number | string;
+    position: {
+      x: number;
+      y: number;
+    };
+    indexPlayer:
+      | number
+      | string /* id of the player in the current game session */;
+  };
+}
+
+export interface AttackResponseMessage extends Message {
+  type: 'attack';
+  data: {
+    position: {
+      x: number;
+      y: number;
+    };
+    currentPlayer:
+      | number
+      | string /* id of the player in the current game session */;
+    status: 'miss' | 'killed' | 'shot';
+  };
+}
+
+export interface RandomAttackRequestMessage extends Message {
+  type: 'randomAttack';
+  data: {
+    gameId: number | string;
+    indexPlayer:
+      | number
+      | string /* id of the player in the current game session */;
+  };
+}
+
+export interface PlayersTurnInfoResponseMessage extends Message {
+  type: 'turn';
+  data: {
+    currentPlayer:
+      | number
+      | string /* id of the player in the current game session */;
+  };
+}
+
+export interface FinishGameResponseMessage extends Message {
+  type: 'finish';
+  data: {
+    winPlayer:
+      | number
+      | string /* id of the player in the current game session */;
+  };
+}
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -5,7 +5,24 @@ export default function startWebSocketServer(port: number) {
 
     wss.on('connection', function connection(ws) {
         ws.on('message', function incoming(message) {
-            console.log('received: ', message);
+            const messageString = message.toString();
+            try {
+                const parsedMessage = JSON.parse(messageString);
+                console.log('Received message:', parsedMessage);
+            } catch (error) {
+                console.error('Error parsing message:', error);
+            }
         })
+
+        ws.onclose = (event) => {
+            console.log('WebSocket server closed');
+        };
+    })
+
+
+    process.on('SIGINT', () => {
+        wss.close(() => {
+            process.exit(0);
+        });
     })
 }
