0041ea92fe0076ea68a9f4facbc8770ffa2d70d5
amgstrider <amgstrider@gmail.com>
2024-02-25 11:28:42 +0400

    refactor: remove unnececcary handleCreateRoom

--- src/ws_server/handleRequest.ts
+++ src/ws_server/handleRequest.ts
@@ -1,8 +1,9 @@
 import { WebSocket } from "ws";
 import { Message, RegRequestData } from "./common/interfaces";
 import { handleReqRequest } from "./handleReqRequest";
-import { handleCreateRoomReq } from "./handleCreateRoomReq";
-import { CurrentUser } from "./common/GameDatabase";
+import { CurrentUser, GameDatabase } from "./common/GameDatabase";
+
+const gameDatabase = GameDatabase.getInstance();
 
 export default function handleRequest(message: Message, ws: WebSocket, currentUser: CurrentUser) {
   const messageData = message.data.length ? JSON.parse(message.data) as RegRequestData : {} as RegRequestData;
@@ -10,7 +11,7 @@ export default function handleRequest(message: Message, ws: WebSocket, currentUs
     case 'reg':
       return handleReqRequest(messageData, ws);
     case 'create_room':
-      handleCreateRoomReq(ws, currentUser);
+      gameDatabase.createRoom(currentUser);
       break;
   }
 }
